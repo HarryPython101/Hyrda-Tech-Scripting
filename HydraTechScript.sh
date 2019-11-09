051b6339a5a86b710863777136f18a62b1a4ff7e
Eric Mayo <ericmmrocks@gmail.com>
2019-11-09 13:08:41 -0500

    added disclaimer to beginning of script

--- HydraTechScript.sh
+++ HydraTechScript.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+
+#Hyrda Tech Scripting
+
+#This script has been created by Eric Mayo exclusively for the use of the Gull Lake High School Cyberpatriot team HydraTech .
+#use of this script by any other teams or people for the cyberpatriot competition is strictly prohibited. If you do so you accept all
+#responsibility for any rule breaches you may cause and the consequences of such. If you wish to use my script as a reference to write
+#your own, that is perfectly acceptable. However I would request that you at least make an effort to understand what they commands
+#do and why they are there before using them.
+
+
 BLUE='\033[0;34m'
 RED='\033[0;031m'
 GREEN='\033[0;032m'
